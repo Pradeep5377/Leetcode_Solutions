3a3e1e20d6b31f68134c5c12b17d6f43014bb762
Pradeep T M <pradeeptm5377@gmail.com>
2025-03-25 10:20:34 +0530

    Time: 3 ms (93.89%), Space: 43.2 MB (88.75%) - LeetHub

---
+++ 1532-reformat-the-string/1532-reformat-the-string.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String reformat(String s) {
+        int charcount = 0;
+        int digitcount = 0;
+        StringBuilder chr = new StringBuilder();
+        StringBuilder digit = new StringBuilder();
+        StringBuilder ans = new StringBuilder();
+        for(char c :s.toCharArray()){
+            if(c>='a' && c<='z'){
+                charcount++;
+                chr.append(c);
+            }
+            else{
+                digitcount++;
+                digit.append(c);
+            }
+        }
+        if(Math.abs(charcount-digitcount)>1) return "";
+
+        boolean letterFirst = charcount >= digitcount;
+        int i = 0;
+        while(i<charcount && i<digitcount){
+            if(letterFirst){
+                ans.append(chr.charAt(i));
+                ans.append(digit.charAt(i));
+            }
+            else{
+                ans.append(digit.charAt(i));
+                ans.append(chr.charAt(i));
+            }
+            i++;
+        }
+
+        if(charcount>i){
+            ans.append(chr.charAt(i));
+        }
+        else if(digitcount>i){
+            ans.append(digit.charAt(i));
+
+        }
+
+        if(charcount==s.length()/2 && digitcount==s.length()/2) return s;
+
+
+    return ans.toString();
+    }
+}
