0ad3c11f8171a2896f7d766854103b181e66824b
Pradeep T M <pradeeptm5377@gmail.com>
2025-03-28 05:04:02 +0530

    Time: 21 ms (13.08%), Space: 48.1 MB (36.49%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,13 +3,20 @@ class Solution {
         Map<String,List<String>> result = new HashMap<>();
 
         for(String s : strs){
-            char[] ch = s.toCharArray();
-            Arrays.sort(ch);
-            String key = new String(ch);
+            int count[] = new int[26];
+            for(char c : s.toCharArray()){
+                count[c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i : count){
+                sb.append(i).append("$");
+            }
+            String key = sb.toString();
             if(!result.containsKey(key)){
                 result.put(key,new ArrayList<>());
             }
             result.get(key).add(s);
+
         }
         return new ArrayList<>(result.values());
     }
