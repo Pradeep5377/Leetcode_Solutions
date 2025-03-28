06f25894db23e0682cbece605f62dcb4b2961864
Pradeep T M <pradeeptm5377@gmail.com>
2025-03-28 05:50:11 +0530

    Time: 7 ms (67.65%), Space: 42.1 MB (52.84%) - LeetHub

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.java
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean areOccurrencesEqual(String s) {
-        int[] freq = new int[26];
-        for(char chr:s.toCharArray()){
-            freq[chr-'a']++;
-        }   
-        int len = -1;
-        // int check = 1;
-        for(int i=0;i<26;i++){
-            if(freq[i]>0 ){
-                len = freq[i];
-                break;
-            }
+        Map<Character,Integer> map = new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
         }
-            System.out.println(len);
-        for(int i =0;i<26;i++){
-            System.out.println(freq[i]);
-            if(freq[i]!=len && freq[i]!=0){
+        int firstValue = -1;
+        boolean first = true;
+        for(int val : map.values()){
+            if(first){
+                firstValue = val;
+                first = false;
+            }
+            else if(val!=firstValue){
                 return false;
             }
         }
