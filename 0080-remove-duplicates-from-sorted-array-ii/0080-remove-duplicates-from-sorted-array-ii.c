fcf8feeb5e05a01fa5b29a83b53b09b7fc83aa5d
Pradeep T M <pradeeptm5377@gmail.com>
2025-04-03 03:48:49 +0530

    Time: 23 ms (88.92%), Space: 14.4 MB (86.52%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.c
@@ -0,0 +1,11 @@
+int removeDuplicates(int* nums, int numsSize) {
+    if(numsSize<2) return numsSize;
+
+    int ptr = 2;
+    for(int i=2;i<numsSize;i++){
+        if(nums[i]!=nums[ptr-2]){
+            nums[ptr++] = nums[i];
+        }
+    }
+    return ptr;
+}
