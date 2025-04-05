f304f8155954dae4beece85016ced81aa4f4163f
Pradeep T M <pradeeptm5377@gmail.com>
2025-04-05 15:01:50 +0530

    Time: 0 ms (100%), Space: 12 MB (94.78%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -1,10 +1,11 @@
 int removeDuplicates(int* nums, int numsSize) {
-    int ptr = 0; //Pointer1
-    for(int i=1;i<numsSize;i++)//i=pointer2;
-    {
-        if(nums[i]!=nums[ptr]){
-            nums[++ptr]=nums[i];
+    int index = 0;
+    for(int i = 0;i<numsSize;i++){
+        // int val = nums[index];
+        if(nums[i]!=nums[index]){
+            // index++;
+            nums[++index]=nums[i];
         }
     }
-    return ptr+1;//Size of the result array;
+    return ++index;
 }
