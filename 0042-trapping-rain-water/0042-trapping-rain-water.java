87b6f7a9d870547d5ff839085ae79cc3f664fb6e
Pradeep T M <pradeeptm5377@gmail.com>
2025-04-11 06:31:33 +0530

    Time: 7 ms (5.14%), Space: 45.5 MB (98.62%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int trap(int[] height) {
+        Stack <Integer> rightmax = new Stack<>();
+        rightmax.push(height[height.length-1]);
+        for(int i=height.length-2;i>=2;i--){
+            rightmax.push(Math.max(rightmax.peek(),height[i]));
+        }
+
+        int leftmax = height[0];
+        int totalWater = 0;
+        for(int i=1;i<height.length-1;i++){
+
+            int value = Math.min(leftmax,rightmax.peek());
+            int temp = value - height[i];
+            totalWater +=Math.max(0,temp);
+            leftmax = Math.max(leftmax,height[i]);
+            rightmax.pop();
+        }
+        return totalWater;
+    }
+}
