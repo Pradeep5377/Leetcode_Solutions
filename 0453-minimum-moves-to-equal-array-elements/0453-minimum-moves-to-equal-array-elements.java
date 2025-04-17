db71fa502161c7c13211a599771405bc1fe0fbd7
Pradeep T M <pradeeptm5377@gmail.com>
2025-04-17 23:24:14 +0530

    Time: 3 ms (39.2%), Space: 46 MB (39.85%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.java
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.java
@@ -1,15 +1,13 @@
 class Solution {
     public int minMoves(int[] nums) {
-        int sum = 0,max = 0,min = Integer.MAX_VALUE;
+        int sum = 0,min = Integer.MAX_VALUE;
 
         for(int i:nums){
-            // max = Math.max(i,max);
             min = Math.min(i,min);
             sum+=i;
         }
-        // return (max*nums.length) - sum;
         return sum - min*nums.length ;
     }
 }
 
-// We should make all the array elements equals to the Maximum value...So we are multiplying the maximum value with n (length) so that the max value is distributed for all the elements and we are subtracting the avalaible values in the array..... hence we get the remaining moves to make the array equal...
+// We should reduce all the array elements equals to the minimum value...So we are multiplying the minimum value with n (length) so that the min value is distributed for all the elements and we are subtracting the Sum from the min value of the array   h
