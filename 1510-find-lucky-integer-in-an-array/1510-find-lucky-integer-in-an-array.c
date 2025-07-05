8a52e1d4cf19c91654f41e5400146587652b6700
Pradeep T M <pradeeptm5377@gmail.com>
2025-07-05 07:28:21 +0530

    Time: 0 ms (100%), Space: 9 MB (11.46%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.c
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.c
@@ -1,10 +1,11 @@
 int findLucky(int* arr, int arrSize) {
-    int freq[501];
-
+    int freq[501]={0};
+    int index = 0;
     for(int i=0;i<arrSize;i++){
         freq[arr[i]]++;
+        index = (arr[i]>index) ? arr[i] : index;
     }
-    for(int i=500;i>=1;i--){
+    for(int i=index;i>=1;i--){
         if(freq[i]== i){
             return i;
         }
