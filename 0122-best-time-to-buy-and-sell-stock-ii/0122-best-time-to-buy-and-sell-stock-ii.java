d653f6320dc2bfa0519ba202ffcc9d7736f55673
Pradeep T M <pradeeptm5377@gmail.com>
2025-07-09 06:56:36 +0530

    Time: 3 ms (12.04%), Space: 50.7 MB (5.48%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,12 +1,26 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit = 0;
+        Integer[][] dp = new Integer[prices.length][2];
+        return recursion(0, 1, prices, dp);
+    }
 
-        for(int i=1;i<prices.length;i++){
-            if(prices[i]>prices[i-1]){
-                profit+= prices[i]-prices[i-1];
-            }
+    private int recursion(int index, int buy, int[] prices, Integer[][] dp){
+
+        if(index == prices.length) return 0;
+
+        if(dp[index][buy]!= null) return dp[index][buy];
+        int profit = 0;
+        if(buy==1){
+            int buyHere = -prices[index] + recursion(index+1,0,prices,dp);
+            int skipBuy = recursion(index+1, 1, prices,dp);
+            profit += Math.max(buyHere, skipBuy);
         }
+        else{
+            int sellHere = prices[index] + recursion(index+1, 1, prices,dp);
+            int skipSell = recursion(index+1, 0, prices,dp);
+            profit += Math.max(sellHere, skipSell);
+        }
+        dp[index][buy] = profit;
         return profit;
     }
 }
