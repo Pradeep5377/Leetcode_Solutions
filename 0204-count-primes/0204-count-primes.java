a5998cafdeb4125af7c86627ea0127bf2db782db
Pradeep T M <pradeeptm5377@gmail.com>
2025-08-11 17:53:42 +0530

    Time: 103 ms (36.7%), Space: 49.6 MB (32.08%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -5,7 +5,7 @@ class Solution {
         if(n==0 || n==1) return 0;
         seive[0]=seive[1] = false;
 
-        for(int i=2;i*i<n;i++){
+        for(int i=2;i<Math.sqrt(n);i++){
             if(seive[i]){
                 for(int j= i*i;j<n;j+=i){
                     seive[j] = false;
