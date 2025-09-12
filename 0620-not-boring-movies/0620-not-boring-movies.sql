8e92919fdd6e058db7d7611b1c23dc22b46bda8d
Pradeep T M <pradeeptm5377@gmail.com>
2025-09-12 15:40:08 +0530

    Time: 217 ms (92.85%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
-select * from cinema 
-where id%2=1 && description !="boring"
+
+select * 
+from Cinema
+where id%2!=0 and description != 'boring'
 order by rating desc;
